2bd712dc2a3cdbfd78a2be0bcbe7422463776681
trilespharm2 <trilespharm+2@gmail.com>
2026-02-23 12:59:23 -0500

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,7 +2,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
 MAIN_MENU_TEXT = """
-👋 *Welcome to Premium Video Access*
+👋 *Welcome to Natural XXX🍑 Exclusive Video Access*
 
 Please select an option below:
 """
